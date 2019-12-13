0fb4fa57e424ad7fcc38754009b7c995a6d99554
fivehoho75 <fivehoho75@gmail.com>
2019-12-13 16:56:15 +0900

    Update bookinfo.ts

--- src/store/modules/bookinfo.ts
+++ src/store/modules/bookinfo.ts
@@ -1,12 +1,18 @@
-import { createAction, handleActions } from 'redux-actions';
+import { Action, createAction, handleActions } from 'redux-actions';
 import { applyPenders } from 'redux-pender';
 import * as api from '../../lib/api';
 import produce from 'immer';
 
-const GET_BOOKINFO = 'book/GET_BOOKINFO';
+const SET_BOOKINFO_PAGE = 'book/SET_BOOKINFO_PAGE';
+const GET_BOOKINFOS = 'book/GET_BOOKINFOS';
+const PREFETCH_BOOKINFOS = 'book/PREFETCH_BOOKINFOS';
+const REVEAL_PREFETCHED = 'book/REVEAL_PREFETCHED';
 
 export const actionCreators = {
-  getBookInfo: createAction(GET_BOOKINFO, api.getBookInfo),
+  setBookInfoPage: createAction(SET_BOOKINFO_PAGE, (page: number) => page),
+  getBookInfos: createAction(GET_BOOKINFOS, api.getBookInfos),
+  prefetchBookInfos: createAction(PREFETCH_BOOKINFOS, api.getBookInfos),
+  revealPrefetched: createAction(REVEAL_PREFETCHED),
 };
 
 export interface BookItem {
@@ -23,8 +29,17 @@ export interface BookItem {
   translators: [];
   url: string;
 }
+export interface MetaItem {
+  total_count: number;
+  pageable_count: number;
+  is_end: boolean;
+}
+
 export interface BookInfoSet {
   books: BookItem[] | null;
+  meta: MetaItem;
+  prefetched: BookItem[] | null;
+  page: number;
 }
 
 export interface BookInfo {
@@ -33,6 +48,9 @@ export interface BookInfo {
 
 const initialStateSet: BookInfoSet = {
   books: null,
+  meta: { total_count: 0, pageable_count: 0, is_end: true },
+  prefetched: null,
+  page: 1,
 };
 
 const initialState: BookInfo = {
@@ -44,22 +62,58 @@ interface BooksResponseAction {
   payload: {
     data: {
       documents: BookItem[];
+      meta: MetaItem;
     };
   };
-  meta: any;
 }
 
-const reducer = handleActions({}, initialState);
+interface BooksRevealAction {
+  type: string;
+  payload: {
+    data: {
+      documents: BookItem[];
+    };
+  };
+}
+
+const reducer = handleActions(
+  {
+    [SET_BOOKINFO_PAGE]: (state, action: Action<any>) => {
+      return produce(state, draft => {
+        draft.info.page = action.payload;
+      });
+    },
+    [REVEAL_PREFETCHED]: (state, action: BooksResponseAction) => {
+      return produce(state, draft => {
+        const { books, prefetched } = draft.info;
+        if (books && prefetched) {
+          books.push(...prefetched);
+          draft.info.prefetched = null;
+        }
+      });
+    },
+  },
+  initialState
+);
 
 export default applyPenders(reducer, [
   {
-    type: GET_BOOKINFO,
+    type: GET_BOOKINFOS,
     onSuccess: (state, action: BooksResponseAction) => {
       return produce(state, draft => {
-        draft.info = {
-          books: action.payload.data.documents,
-        };
-        console.log('get:', JSON.stringify(draft));
+        draft.info.books = action.payload.data.documents;
+        draft.info.meta = action.payload.data.meta;
+        draft.info.page = state.info.page;
+      });
+    },
+  },
+  {
+    type: PREFETCH_BOOKINFOS,
+    onSuccess: (state, action: BooksResponseAction) => {
+      return produce(state, draft => {
+        draft.info.prefetched = action.payload.data.documents;
+        draft.info.meta = action.payload.data.meta;
+        draft.info.page = state.info.page;
       });
     },
   },
