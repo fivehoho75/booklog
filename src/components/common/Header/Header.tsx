5a86c54828b1ed38fee45f5e04075c7faeaa78e6
fivehoho75 <fivehoho75@gmail.com>
2019-12-31 08:48:38 +0900

    Update Header.tsx

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,17 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Header.scss';
 import { Link } from 'react-router-dom';
+import Button from 'components/common/Button';
 
 const Header = () => {
   return (
     <div className="Header">
-      Header
       <div className="button-area">
         <Link to="/search" className="search-btn">
-          search
+          <span>search</span>
         </Link>
       </div>
       <div className="spacer" />
+      <div className="right-area">
+        <Button theme="outline" to="/book">
+          로그인
+        </Button>
+      </div>
     </div>
   );
 };
